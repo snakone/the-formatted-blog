8e8950a5bbd1d7a0421c464c626bb54e7a945157
Allison <anticscode@gmail.com>
2021-08-21 22:40:27 +0200

    feat(layout): post carrousel

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,16 +5,20 @@ import { HomeRoutingModule } from './home.routing';
 import { HomeComponent } from './home.component';
 import { MainPostComponent } from './components/main-post/main-post.component';
 import { NotificationBarComponent } from './components/notification-bar/notification-bar.component';
+import { PostCarrouselComponent } from './components/post-carrousel/post-carrousel.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
     HomeComponent,
     MainPostComponent,
-    NotificationBarComponent
+    NotificationBarComponent,
+    PostCarrouselComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselModule
   ]
 })
 
