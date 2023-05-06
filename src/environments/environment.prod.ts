6db52255937f5a54592736e8fc1052d9911f36a4
Sergi <snak_one@hotmail.com>
2023-05-06 03:56:19 +0200

    feat(git): add env to gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'https://formatted-blog.herokuapp.com/',
-  keys: {
-    push: 'BKPDjgxW1e-5FkvRtqg9U0ppcp8vKzUCWFM6sKCZ2WPKr8X1FpC18acJYdnityvrOf6jSNtPeC-WjPcRbqoXtsU'
-  }
-};
