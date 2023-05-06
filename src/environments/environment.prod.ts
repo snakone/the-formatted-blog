4920fc2ba34a750cae191ececda1c2808c4078c3
Sergi <snak_one@hotmail.com>
2023-05-06 18:24:27 +0200

    feat(testing): server

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  api: 'https://theformattedblog-snakone.b4a.run/',
+  keys: {
+    push: 'BKPDjgxW1e-5FkvRtqg9U0ppcp8vKzUCWFM6sKCZ2WPKr8X1FpC18acJYdnityvrOf6jSNtPeC-WjPcRbqoXtsU'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  api: 'http://localhost:3000/',
+  keys: {
+    push: 'BKPDjgxW1e-5FkvRtqg9U0ppcp8vKzUCWFM6sKCZ2WPKr8X1FpC18acJYdnityvrOf6jSNtPeC-WjPcRbqoXtsU'
+  }
+};
+
+// api: 'http://localhost:3000/'
+// api: 'https://theformattedblog-snakone.b4a.run/'
